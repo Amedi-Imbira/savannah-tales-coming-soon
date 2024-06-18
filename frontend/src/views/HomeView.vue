1b5d694b281b21f2cb2c68595699a0bc5113dcae
Amedi-Imbira <amedi.imbira@gmail.com>
2024-06-18 20:23:38 +0300

    Built the brand logo

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -11,8 +11,7 @@ export default {
 </script>
 
 <template>
-  <section class="hero is-primary is-medium">
-    <!-- Hero head: will stick at the top -->
+  <section class="hero is-success is-large">
     <div class="hero-head">
       <nav class="navbar">
         <div class="container">
@@ -20,20 +19,20 @@ export default {
             <a class="navbar-item">
               <img src="https://bulma.io/assets/images/bulma-type-white.png" alt="Logo" />
             </a>
-            <span class="navbar-burger" data-target="navbarMenuHeroA">
+            <span class="navbar-burger" data-target="navbarMenuHeroB">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
             </span>
           </div>
-          <div id="navbarMenuHeroA" class="navbar-menu">
+          <div id="navbarMenuHeroB" class="navbar-menu">
             <div class="navbar-end">
               <a class="navbar-item is-active"> Home </a>
               <a class="navbar-item"> Examples </a>
               <a class="navbar-item"> Documentation </a>
               <span class="navbar-item">
-                <a class="button is-primary is-inverted">
+                <a class="button is-info is-inverted">
                   <span class="icon">
                     <i class="fab fa-github"></i>
                   </span>
@@ -46,28 +45,11 @@ export default {
       </nav>
     </div>
   
-    <!-- Hero content: will be in the middle -->
     <div class="hero-body">
       <div class="container has-text-centered">
         <p class="title">Title</p>
         <p class="subtitle">Subtitle</p>
       </div>
-    </div>
-  
-    <!-- Hero footer: will stick at the bottom -->
-    <div class="hero-foot">
-      <nav class="tabs">
-        <div class="container">
-          <ul>
-            <li class="is-active"><a>Overview</a></li>
-            <li><a>Modifiers</a></li>
-            <li><a>Grid</a></li>
-            <li><a>Elements</a></li>
-            <li><a>Components</a></li>
-            <li><a>Layout</a></li>
-          </ul>
-        </div>
-      </nav>
     </div>
   </section>
 </template>
